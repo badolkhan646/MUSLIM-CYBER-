8b95ceff7c653040673386d8b5f9ca80bc643050
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-04-03 20:55:31 +0600

    Update Boss-bj.js

--- scripts/commands/Boss-bj.js
+++ scripts/commands/Boss-bj.js
@@ -1,27 +1,31 @@
 const fs = require("fs");
-module.exports.config = {
-	    name: "boss",
-    version: "1.1.0",
-    permission: 0,
-    credits: "BADOL-KHAN",
-    description: "noprefix",
-    prefix: true,
-    category: "commands",
-    usages: "boss",
-    cooldowns: 5,
-};
+module.exports = {
+  config:{
+	name: "bossbj",
+        version: "1.0.1",
+        prefix: false,
+	permssion: 0,
+	credits: "BADOL-KHAN", 
+	description: "Fun",
+	category: "no prefix",
+	usages: "bossbj",
+        cooldowns: 5, 
+},
 
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+handleEvent: function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("Raja vai")==0 || event.body.indexOf("100004504180813")==0 || event.body.indexOf("@Raja Babu _____// ;* :/ 3:)")==0 || event.body.indexOf("100007070042228")==0) {
+  const content = event.body ? event.body : '';
+  const body = content.toLowerCase();
+	if (body.indexOf("Raja babu")==0 || body.indexOf("@Raja Babu _____// ;* :/ 3:)")==0 || body.indexOf("100007070042228")==0 || body.indexOf("Raja vai")==0) {
 		var msg = {
-				body: "📌 Boss Ekhon BG Ok",
+				body: "😑রা্ঁজা্ঁ-বা্ঁবু্ঁ-কে-এতো-ডাকো-কেনো🙂\n😒জাননা-রা্ঁজা্ঁ-বা্ঁবু্ঁ-আমার-চিপাই-থাকে🙄",
 				attachment: fs.createReadStream(__dirname + `/BADOL-KHAN/22.mp3`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("🤦‍♂️", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🤙", event.messageID, (err) => {}, true)
 		}
-	}
-	module.exports.run = function({ api, event, client, __GLOBAL }) {
+	},
+	start: function({ nayan }) {
 
-}
+  }
+	}
