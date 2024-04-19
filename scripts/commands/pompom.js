c7462bc647ceb256f55d28fdc112adfbb979ba96
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-04-19 15:37:39 +0600

    Update pompom.js

--- scripts/commands/pompom.js
+++ scripts/commands/pompom.js
@@ -17,26 +17,26 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["😬 পমপম দেখো ভালো 😘\n🖐️হাত মারাটা ভালোনা ধন্যবাদ 🤦‍♂️"];
+   var hi = ["😬 পমপম দেখেন ভালো কথা 😘\n🖐️হাত মেরে নুনুকে বমিবমি করবেন না ধন্যবাদ 🤦‍♂️"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
-  "https://i.imgur.com/frizc8X.jpeg",
-"https://i.imgur.com/frizc8X.jpeg",
-"https://i.imgur.com/Gr2VqSY.jpeg",
-"https://i.imgur.com/2brxSWx.jpeg",
-"https://i.imgur.com/R0zkPs9.jpeg",
-"https://i.imgur.com/uVnrUCZ.jpeg",
-"https://i.imgur.com/4II2mWR.jpeg",
-"https://i.imgur.com/p7nXG8H.jpeg",
-"https://i.imgur.com/UxUmOjT.jpeg",
-"https://i.imgur.com/vmvJjhU.jpeg",
-"https://i.imgur.com/d9AgdrX.jpeg",
-"https://i.imgur.com/fIU4TVX.jpeg",
-"https://i.imgur.com/r7X9UnR.jpeg",
-"https://i.imgur.com/l7dv4vA.jpeg",
-"https://i.imgur.com/BeW82jj.jpeg",
-"https://i.imgur.com/BeW82jj.jpeg",
-"https://i.imgur.com/l7dv4vA.jpeg",
+"https://drive.google.com/uc?id=1vSzdo3KfurhSemOuZAZdRB9NtxNGM-9J",
+"https://drive.google.com/uc?id=1uvbVgagg9YOMTyW0Uw-Tk6qUx7teuFxb",
+"https://drive.google.com/uc?id=1vHa-RpL8ZEziei2_GvAhjcSPdSgPkgoz",
+"https://drive.google.com/uc?id=1uubhSCCzKRvq2ZgGVLDwvz01UHmQLK9G",
+"https://drive.google.com/uc?id=1v0K_GuQfUTAvlcdEGJXcm_bSQ-eozmPv",
+"https://drive.google.com/uc?id=1vUqWEIFS9UYZoyD46IL1DN4_nmMA-rOk",
+"https://drive.google.com/uc?id=1vZ9SrycMxGGaDgg2z2i5twNbCs5aidqn",
+"https://drive.google.com/uc?id=1vYPkxsR5vHC34g1e7Q3Li2Z9vWeHHtvp",
+"https://drive.google.com/uc?id=1vBy4FlK759A_SoldXqovos9D1y88ZZFv",
+"https://drive.google.com/uc?id=1ux37NoP7UzBKYBNh3XLCusDQE73VGdXh",
+"https://drive.google.com/uc?id=1uzHPq0vT3vhYlzxTARx_chaHsGh9nEzp",
+"https://drive.google.com/uc?id=1v7RqS-hso8s2247NLsVTFxMgkAH-GKrj",
+"https://drive.google.com/uc?id=1uj0ffc64H3bW-4Jp_VCeoI3qgFLRLmrl",
+"https://drive.google.com/uc?id=1uo2-8diGD4iTm4WJ8WXww0vKknMrQtmD",
+"https://drive.google.com/uc?id=1vT1x1XXrl8xNFOOtXwJpKOsF-VFeVQCs",
+"https://drive.google.com/uc?id=1v5BjRD27kYT8xWcCfPPYutyd7azhLGTJ",
+"https://drive.google.com/uc?id=1vT1x1XXrl8xNFOOtXwJpKOsF-VFeVQCs",
 ];
 	 var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
