f9500a224ed70a47ec7945308565a621bb217861
badolkhan646 <141847046+badolkhan646@users.noreply.github.com>
2024-04-26 23:41:22 +0600

    Create maymuna.js

---
+++ scripts/commands/maymuna.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+const jimp = require("jimp");
+const fs = require("fs");
+module.exports.config = {
+  name: "maymuna",
+  version: "1.0.2",
+  permission: 0,
+  credits: "BADOL-KHAN",
+  description: "beginner's guide",
+  prefix: false,
+  category: "guide",
+  usages: "[Shows Commands]",
+  cooldowns: 5,
+  envConfig: {
+    autoUnsend: true,
+    delayUnsend: 60
+  }
+};
+
+module.exports.run = function ({ event, api }) {
+    const t = Date.parse("November 25, 2025 00:00:00") - Date.parse(new Date());
+    const seconds = Math.floor( (t/1000) % 60 );
+    const minutes = Math.floor( (t/1000/60) % 60 );
+    const hours = Math.floor( (t/(1000*60*60)) % 24 );
+    const days = Math.floor( t/(1000*60*60*24) );
+
+    return api.sendMessage(`╭•┄┅════❁𝙈𝘾𝙎•𝟬𝟬𝟳❁════┅┄•╮\n\n★মাইমুনার জন্মদিন প্রর্যন্ত বাকি সময় হলো★\n\n»${days} ★দিন★ \n\n${hours} ★ঘন্টা★ \n\n${minutes} ★মিনিট★ \n\n${seconds} ★সেকেন্ড★\n\n╰•┄┅════❁𝙈𝘾𝙎•𝟬𝟬𝟳❁════┅┄•╯`,
+event.threadID, event.messageID);
+                                 }
